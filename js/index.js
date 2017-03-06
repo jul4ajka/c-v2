ebacef6078a40f07b36bbb9d742279aa8de156f8
JuliaChayka <juliachayka133@yahoo.com>
2017-03-06 15:46:18 +0000

    New cv

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var images = [
+  "images/html-course.jpeg", 
+  "images/css-course.jpeg", 
+  "images/javascript-course.jpeg"
+  ];
+  var num = 0;
+
+function next() {
+  var slider = document.getElementById("slider");
+  num++;
+  if(num >= images.length) {
+    num = 0;
+  }
+  slider.src = images[num];
+  }
+
+function prev() {
+  var slider = document.getElementById("slider");
+  num--;
+  if(num < 0) {
+    num = images.length-1;
+  }
+  slider.src = images[num];
+}
