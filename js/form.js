052b1b404f70e2f86c906d07456896477178712e
JuliaChayka <juliachayka133@yahoo.com>
2017-03-10 16:53:34 +0000

    new

---
+++ js/form.js
@@ -0,0 +1,18 @@
+$(function() {
+   $('#btn-submit').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/jul4ajka@gmail.com", 
+            method: "POST",
+            data: {
+                firstName: $('#first-name').val(),
+                lastName: $('#last-name').val(),
+                visitorMail: $('#visitor-email').val(),
+                comment: $('#comment').val()
+            },
+            dataType: "json"
+        }).done(function() {
+            $('form').html('<h1>I appreciate your opinion!</h1><p>Thank you for feedback</p>')
+        });
+   }); 
+});
